f71d1096873f4948e74aec09c6de6e1af82e598a
Charaf Chaouki <charaf.chawki1999@gmail.com>
2022-09-24 16:48:21 +0100

    Prepare the structure of the project

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//Variable Application
+$dir = __DIR__ . '/files/';
+$filePath = $dir . 'file1.txt';
+
+//Check file exists
+if(file_exists($filePath))
+{
+    //Open File
+    //$file = fopen($filePath, 'r');
+
+   // if($file)
+    {
+        //read content
+        $html = file_get_contents($filePath);
+        
+        echo nl2br($html);
+
+        //close File
+       // fclose($file);
+    }
+}
+else
+{
+    die('This file doesn\'t exist!');
+}
